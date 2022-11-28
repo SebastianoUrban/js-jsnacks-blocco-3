27b737a1539c4bafdb61d848a7eab3c7d218e12c
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-11-28 16:41:20 +0100

    version 2

--- snack_2/js/script.js
+++ snack_2/js/script.js
@@ -1,10 +1,13 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 let somma = 0;
+const numbers = [];
 
 while (somma < 50) {
     const userNumber = parseInt(prompt('inserisci un numero'));
     somma += userNumber;
+    numbers.push(userNumber)
 }
 
+console.log(numbers);
 console.log('La somma è : ' + somma)
